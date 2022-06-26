7bbbca03b5f7bca3cb7299cb2bb485054cb24978
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-26 14:28:26 +0300

    Добавьте файлы проекта.

---
+++ CS15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "Изподвыподверта";
+            int tryCount = 3;
+            string userInput;
+            
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (password == userInput)
+                {
+                    Console.Write("Секретное сообщение");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный пароль. У вас осталось " + (tryCount - 1 - i) + " попытки");
+                }  
+            }
+        }
+    }
+}
